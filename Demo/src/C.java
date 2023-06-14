e07cc1d6ff33413316d72da75c19b6bc555baad8
MSR7097 <madhusudhankodumuru@gmail.com>
2023-06-14 11:38:58 +0530

    NBK

---
+++ Demo/src/C.java
@@ -0,0 +1,17 @@
+class C 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main begins");
+		
+		else
+		{
+			System.out.println("from else");
+		}
+		
+		
+		System.out.println("main end!");
+	}
+}
+
+
